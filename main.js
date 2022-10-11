30e7060362cb718e6e3e27112cf36bf708b8cd28
Allan <amoua4@ucsc.edu>
2022-10-10 19:30:18 -0700

    added characters

--- main.js
+++ main.js
@@ -1,15 +1,135 @@
-title = "";
+title = 'DOWNHILL SKI';
 
 description = `
 `;
 
-characters = [];
+characters = [
+//player char 'a' 'b'
+`
+ yyr
+ rr
+bbbb
+cbbc
+LbbL
+LBBL
+`,
+`
+ yy
+ rr
+bbbb
+cbbc
+LbbL
+LBBL
+`,
+//skis char 'c' 'd'
+`
+gg
+gg
+gg
+gg
+`,
+`
+    gg
+   gg
+  gg
+ gg 
+`,
+//trees char 'e'
+` 
+  G
+ GGG
+GGGGG
+ GGG
+GGGGG
+  L
+`,
+//rock char 'f'
+`
+  LL
+ LLLL
+LLLLLL
+LLLLLL
+`,
+//rival snowboarders char 'f'
+`
+  YY
+  YY
+ PPPP
+ pPPp
+  CC
+  CC
+`
+];
 
-options = {};
+const G = {
+  WIDTH: 75,
+	HEIGHT: 100,
+
+  SNOW_SPEED_MIN: 0.05,
+  SNOW_SPEED_MAX: 0.2,
+};
+
+let player;
+let anim; //animation default = 0, turn left = 1, turn right = 2
+
+options = {
+  theme: "dark",
+  isPlayingBgm: true,
+  seed: 332894,
+	viewSize: {x: G.WIDTH, y: G.HEIGHT} 
+};
 
 function update() {
   if (!ticks) {
+    player = vec(G.WIDTH/2, G.HEIGHT/3);
+    anim = 0;
+
+    snow = times(10, () => {
+      // Random number generator function
+      // rnd( min, max )
+      const posX = rnd(0, G.WIDTH);
+      const posY = rnd(0, G.HEIGHT);
+      // An object of type Star with appropriate properties
+      return {
+          // Creates a Vector
+          pos: vec(posX, posY),
+          // More RNG
+          speed: rnd(G.SNOW_SPEED_MIN, G.SNOW_SPEED_MAX)
+      };
+    });
   }
+
+  //ground
+  color("black");
+  rect(0, 20, G.WIDTH, G.HEIGHT);
+
+  snow.forEach((s) => {
+    s.pos.y += s.speed;
+    if (s.pos.y > G.HEIGHT - 80) s.pos.y = 0;
+    color("black");
+    box(s.pos, 1);
+  });
+
+  //player and skis
+  color("black")
+  if(anim == 0) {
+    char("c", player.x, player.y + 3);
+    } else if(anim == 1) {
+      char("d", player.x, player.y + 3);
+    } else if(anim == 2) {
+      char("d", player.x, player.y + 3, {mirror: {x: -1}});
+    }
+  char(addWithCharCode("a", Math.floor(ticks / 30) % 2), player);
+
+  
+  //obstacles for reference
+  char("e", 20, 30); //tree
+  char("f", 20, 40); //rock
+  char("c", 61, 43); //snow
+  char("g", 60, 40); //snowboarder
+  
+   
+
 }
 
 addEventListener("load", onLoad);
